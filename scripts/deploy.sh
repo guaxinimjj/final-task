a7d66bb698914f0aaaf3d54b4fafa9d0ce7575dd
Vitalii Koval <vitaliy.musubi@gmail.com>
2020-11-25 15:25:49 +0200

    Update deploy scripts.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 HEROKU_API_KEY=$HEROKU_API_KEY heroku container:login
-HEROKU_API_KEY=$HEROKU_API_KEY heroku container:push    web --app setup-practice
-HEROKU_API_KEY=$HEROKU_API_KEY heroku container:release web --app setup-practice
+HEROKU_API_KEY=$HEROKU_API_KEY heroku container:push    web --app department-app-kv
+HEROKU_API_KEY=$HEROKU_API_KEY heroku container:release web --app department-app-kv
