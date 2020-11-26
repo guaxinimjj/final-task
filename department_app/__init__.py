2ce186fd86d816021ce133878950b4cdc31156fa
Vitalii Koval <vitaliy.musubi@gmail.com>
2020-11-26 12:38:43 +0200

    Add migration

--- department_app/__init__.py
+++ department_app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 from department_app.views import departments
+
+db = SQLAlchemy()
 
 
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
     app.register_blueprint(departments.bp)
+    db.init_app(app)
+
     return app
